54f19930e26e2a5c31eb7d5283215462d3358dcc
agus4385 <aguskurniawan@students.amikom.ac.id>
2022-04-07 20:35:21 +0700

    tugas5

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overloading_Method
+{
+    class Numbers
+    {
+        public int FindMinimum(int number1, int number2)
+        { //! Method 1
+            if(number1 > number2)
+            {
+                return number2;
+            }
+            else
+            {
+                return number1;
+            }
+        }
+        public int FindMinimum(int number1, int number2, float number3)
+        {
+            if(number1 < number2 && number1 < Convert.ToInt32(number3))
+            {
+                return number1;
+            }
+            else if(number1 > number2 && number2 < Convert.ToInt32(number3))
+            {
+                return number2;
+            }
+            else
+            {
+                return Convert.ToInt32(number3);
+            }
+        }
+        public int FindMaximum(int number1, int number2)
+        {
+            if (number1 > number2)
+            {
+                return number1;
+            }
+            else
+            {
+                return number2;
+            }
+        }
+        public int FindMaximum(int number1, int number2, float number3)
+        {
+            if (number1 > number2 && number1 > Convert.ToInt32(number3))
+            {
+                return number1;
+            }
+            else if (number1 < number2 && number2 > Convert.ToInt32(number3))
+            {
+                return number2;
+            }
+            else
+            {
+                return Convert.ToInt32(number3);
+            }
+        }
+    }
+}
